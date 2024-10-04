3fca4dc232fecd0950a43a4d5ba53372b867c992
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-04 19:35:28 +0100

    Question 2 terminer

--- Exo1.js
+++ Exo1.js
@@ -7,3 +7,12 @@ function fullName() {
     console.log(name);
 }
 fullName();
+
+
+// Reponse numero 2
+function fullName(firstName = 'Mohamed',secondName = 'Attahir'){
+    let space = ' ';
+    let fullName = firstName + space + secondName
+    console.log(fullName);
+}
+fullName();
