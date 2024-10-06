1d978203df5ed770839673aad0366828c7a4d063
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-06 20:48:37 +0100

    Question 2n terminer:L'équation quadratique est calculée comme suit : ax2 + bx + c = 0. Écrivez une fonction qui calcule la valeur ou les valeurs d'une équation quadratique, solveQuadEquation .
    console.log(solveQuadratic()) // {0}
    console.log(solveQuadratic(1, 4, 4)) // {-2}
    console.log(solveQuadratic(1, -1, -2)) // {2, -1}
    console.log(solveQuadratic(1, 7, 12)) // {-3, -4}
    console.log(solveQuadratic(1, 0, -4)) //{2, -2}
    console.log(solveQuadratic(1, -1, 0)) //{1, 0}

--- Exo2.js
+++ Exo2.js
@@ -3,4 +3,27 @@ function solveLinEquation(a,b,c,x,y) {
     équationLinéaire =  (a * x) + (b * y) + c 
     console.log(équationLinéaire);
 }
-solveLinEquation(1,3,6,7,20)
+solveLinEquation(1,3,6,7,20); 
+
+
+// Réponse numero 2
+function solveQuadEquation(a,b,c) {
+    const discriminant = Math.sqrt(b * b - 4 * a * c)
+    if (discriminant > 0) {
+        x1 = -b - discriminant / 2 * a;
+        x2 = -b + discriminant / 2 * a;
+        return {x1, x2}
+    } else if (discriminant < 0) {
+        s = x < 0;
+        return {s}
+    } else if (discriminant === 0) {
+        S = -b / 2 * a
+        return {S}
+    }
+};
+console.log(solveQuadEquation(2, 2, 2));
+console.log(solveQuadEquation(1, 4, 4));
+console.log(solveQuadEquation(1, -1 ,-1));
+console.log(solveQuadEquation(1, 7, 12));
+console.log(solveQuadEquation(1, 0, -4));
+console.log(solveQuadEquation(1, -1, 0));
