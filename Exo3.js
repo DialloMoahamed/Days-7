ec32a50476c6268d3ea7909a75fd1fa66361e86c
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-11 00:20:30 +0100

    Question 2 terminer:Écrivez une fonction nommée rgbColorGenerator et elle génère des couleurs RVB.
    rgbColorGenerator()
    rgb(125,244,255)

--- Exo3.js
+++ Exo3.js
@@ -1,4 +1,4 @@
-// Réponse numero 18
+// Réponse numero 1
 const lettres = 'AZERTYUIJH12345678GFDSQDFYUIJHGFDER123456789TYUwxcvghjkloiuytrdfghjiiuezqsf123456789ghhjoigycdsufduzgf123456789fèuzuyzuyffzdzd';
 let randomId = '';
 let randomId2 = '';
@@ -17,3 +17,14 @@ function userIdGenerator() {
     console.log(randomId2);
 }
 console.log(userIdGenerator());
+
+
+// Réponse numero 2
+function rgbColorGenerator(couleur) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    couleur = "rgb(" + r + "," + g + "," + b + ")";
+    return couleur
+}
+console.log(rgbColorGenerator());
